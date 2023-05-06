c22be9498dd776c3e88c0e30ad0f220f93404666
Andrey Damasceno <andrey.damasceno@outlook.com>
2023-05-06 09:35:12 -0300

    nova modificação

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,11 +1,9 @@
 public class Condicional {
     public static void main(String[] args) {
 
-        System.out.println("Olá Mundo!");
-        int numero = 2;
-        System.out.println(numero);
-        int numero2 = numero + 2;
-        System.out.println(numero2);
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
 
     }
 }
