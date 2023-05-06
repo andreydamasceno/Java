05c6b32d68069950444540d7f2f79c2a30b5df72
Andrey Damasceno <andrey.damasceno@outlook.com>
2023-05-06 09:56:59 -0300

    nova modificação

--- src/Condicional.java
+++ src/Condicional.java
@@ -2,8 +2,20 @@ public class Condicional {
     public static void main(String[] args) {
 
         int anoDeLancamento = 2022;
-        boolean incluidoNoPlano = true;
+        boolean incluidoNoPlano = false;
         double notaDoFilme = 8.1;
+        String tipoDoPlano = "Plus";
 
+        if (anoDeLancamento >= 2022) {
+            System.out.println("Este é um lançamento recente!");
+        } else {
+            System.out.println("Esse é um filme antigo!");
+        }
+
+        if (tipoDoPlano.equals("Plus") || incluidoNoPlano) {
+            System.out.println("Você pode assistir a esse filme a qualquer momento!");
+        } else {
+            System.out.println("Este filme não está incluso no seu plano!");
+        }
     }
 }
