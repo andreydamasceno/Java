7b0718a0dea789890305ac97b47a16482147b8ed
Andrey Damasceno <andrey.damasceno@outlook.com>
2023-05-07 12:44:31 -0300

    nova modificação

---
+++ src/JogoDeAdivinha.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+public class JogoDeAdivinha {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(10);
+
+        for (int tentativas = 0; tentativas < 5; tentativas++) {
+            System.out.println("tente adivinhar o número:");
+            int numeroPensado = leitura.nextInt();
+            if (numeroPensado == numeroAleatorio){
+                System.out.println("Parabéns, você acertou o número!");
+                break;
+            } else {
+                System.out.println("Número errado!");
+            }
+        }
+    }
+}
